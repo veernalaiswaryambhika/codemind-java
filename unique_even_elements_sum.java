4209bb97c52c4824e08bccd1f7553010d5ad07bf
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-08-22 11:23:45 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         a[i]=s.nextInt();         int sum=0;         Arrays.sort(a);         for(int i=0;i<n-1;i++)         {         if(a[i]!=a[i+1]&&a[i]%2==0)         sum=sum+a[i];         }         if(a[n-1]%2==0)         sum=sum+a[n-1];           System.out.println(sum);     }
    }

---
+++ unique_even_elements_sum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        a[i]=s.nextInt();
+        int sum=0;
+        Arrays.sort(a);
+        for(int i=0;i<n-1;i++)
+        {
+        if(a[i]!=a[i+1]&&a[i]%2==0)
+        sum=sum+a[i];
+        }
+        if(a[n-1]%2==0)
+        sum=sum+a[n-1];
+          System.out.println(sum);
+    }
+  
+}
