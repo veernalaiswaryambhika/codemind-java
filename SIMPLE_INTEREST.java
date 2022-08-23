22e5806005726f0be8c3e5d46482c238a29d47b8
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-08-23 23:23:07 +0530

    import java.util.*; class Sam {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);
               int p=s.nextInt();
              int r=s.nextInt();
             int t=s.nextInt();
             int amount=(p*t*r)/100;
                 
                 
                System.out.println(amount);
                
            
        }
      
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Sam
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        
+           int p=s.nextInt();
+          int r=s.nextInt();
+         int t=s.nextInt();
+         int amount=(p*t*r)/100;
+             
+             
+            System.out.println(amount);
+            
+        
+    }
+  
+}
