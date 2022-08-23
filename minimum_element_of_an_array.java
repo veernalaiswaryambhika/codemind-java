2f454a54e2cf7f0de30274fa97ba125c1a9f6e2f
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-08-23 19:09:45 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         a[i]=s.nextInt();         int min=0; int temp=a[0]; 	 for(int i=0;i<n;i++)
    	{
    	 if(temp>a[i])
    	temp=a[i];
    				
    				
    }
    
            System.out.println(temp);
        }
    }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        a[i]=s.nextInt();
+        int min=0;
+int temp=a[0];
+	 for(int i=0;i<n;i++)
+
+	{
+	 if(temp>a[i])
+	temp=a[i];
+				
+				
+}
+
+        System.out.println(temp);
+    }
+}
