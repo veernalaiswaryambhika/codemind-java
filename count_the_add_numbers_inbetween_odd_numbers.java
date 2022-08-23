4d4df8cc46659c89e6d37c247e5b6cf71e3b1c3e
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-08-23 09:23:18 +0530

    import java.util.*; class sol {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)             a[i]=s.nextInt();         int c=0;         for(int i=0,j=1,k=2;k<n;k++,j++,i++)         {             if(a[i]%2!=0 && a[j]%2!=0 && a[k]%2!=0 )             c++;         }         System.out.print(c);     } }

---
+++ count_the_add_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=s.nextInt();
+        int c=0;
+        for(int i=0,j=1,k=2;k<n;k++,j++,i++)
+        {
+            if(a[i]%2!=0 && a[j]%2!=0 && a[k]%2!=0 )
+            c++;
+        }
+        System.out.print(c);
+    }
+}
