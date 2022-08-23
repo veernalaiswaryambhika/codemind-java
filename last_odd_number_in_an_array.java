2db7a8dafcb665a5041494a3876102cf79f848bd
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-08-23 16:56:29 +0530

    import java.util.*; class sol {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];         for(int  i=0;i<n;i++)         a[i]=s.nextInt();         int i,t=0;
               for(i=0;i<n;i++)
               {
               if(a[i]%2!=0)
               t=a[i];
               
           }
            System.out.print(t);
        }
    }

---
+++ last_odd_number_in_an_array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int  i=0;i<n;i++)
+        a[i]=s.nextInt();
+        int i,t=0;
+       
+           for(i=0;i<n;i++)
+           {
+           if(a[i]%2!=0)
+           t=a[i];
+           
+       }
+        System.out.print(t);
+    }
+}
