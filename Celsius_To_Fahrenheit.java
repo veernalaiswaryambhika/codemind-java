e79eff4abe85bd98fb4bcfdc576a6ac7d6e17be8
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-09-04 22:26:15 +0530

    import java.util.*; class Sam {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);
            int c=s.nextInt();
            double d=(1.8)*c +32;
             System.out.printf("%.2f",d);
            
        }
     
    }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sam
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        
+        int c=s.nextInt();
+        double d=(1.8)*c +32;
+         System.out.printf("%.2f",d);
+        
+    }
+ 
+}
