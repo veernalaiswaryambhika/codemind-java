99a3170b74c6c5e612206bd2426d98a82c420b42
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-09-11 21:23:12 +0530

    import java.util.*; class pattern {   public static void main(String args[]) 	{ 	Scanner sc=new Scanner(System.in); 	int R; 	R=sc.nextInt();
    	for(int i=1;i<=R;i++)
    	{
    		for(int j=1;j<=R;j++)
    		{
    		System.out.print(i+" ");
    		}
    		System.out.println();
    	}
    	}
    }

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class pattern
+{
+  public static void main(String args[])
+	{
+	Scanner sc=new Scanner(System.in);
+	int R;
+	R=sc.nextInt();
+	
+	for(int i=1;i<=R;i++)
+	{
+		for(int j=1;j<=R;j++)
+		{
+		System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+	}
+}
