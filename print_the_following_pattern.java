7cee4eec78583ed04b45e7fd5c199cd699c273ec
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-09-12 22:26:00 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();
            for(int i=1;i<=n;i++)
            {
                  for(int j=n;j>0;j--)
                  System.out.print(j+" ");
                      System.out.println();
            }
         
            
             
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,20 +1,19 @@
 import java.util.*;
-class sam
+class sample
 {
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
-        int r=s.nextInt();
-        int t=0;
-        for(int i=0;i<r;i++)
+        int n=s.nextInt();
+        
+        for(int i=1;i<=n;i++)
         {
-             for(int j=1;j<=r;j++)
-             {
-                 System.out.print((char)(65+i)+" ");
-                 
-             }
-             System.out.println();
-            
+              for(int j=n;j>0;j--)
+              System.out.print(j+" ");
+                  System.out.println();
         }
+     
+        
+         
     }
 }
