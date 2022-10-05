44d223a8de0e06765cb283f798dd680be6826dfa
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-10-05 18:48:20 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int sum=0;         int rem=0;        int temp=n;         for(int i=1;i<=n/2;i++)         {             if(n%i==0)             sum+=i;         }
             if(temp==sum)
            
            System.out.println("True");
            else
             System.out.println("False");
        
            
        }
    }

--- Perfect_Number.java
+++ Perfect_Number.java
@@ -1,26 +1,25 @@
 import java.util.*;
-class sol
+class Sample
 {
-    public static boolean perfect(int n)
-    {
-        int t=1;
-        for(int i=2;i<=(int)n/2;i++)
-        {
-            if(n%i==0)
-            t=t+i;
-        }
-        if(t==n)
-        return true;
-        return false;
-    }
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-       if(perfect(n)==true)
+        int sum=0;
+        int rem=0;
+       int temp=n;
+        for(int i=1;i<=n/2;i++)
+        {
+            if(n%i==0)
+            sum+=i;
+        }
+      
+         if(temp==sum)
+        
         System.out.println("True");
-        else 
-            System.out.println("False");
-       
+        else
+         System.out.println("False");
+    
+        
     }
 }
