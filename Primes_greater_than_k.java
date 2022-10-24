4cf980ecaf5c1130e55feb77e003aeddcde8e6c9
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-10-24 23:19:12 +0530

    import java.util.*; class Sample {     public static boolean isprime(int n)    {       if(n==1)       return false;     for(int i=2;i<=Math.sqrt(n);i++)     {        if(n%i==0)        return false;     }      return true;   }     public static void main(String args[])     {         Scanner s=new Scanner(System.in);
                int n=s.nextInt();
       
                int a[]=new int[n];
                for(int i=0;i<n;i++)
                a[i]=s.nextInt();
                int k=s.nextInt();
                int c=0;
                for(int i=0;i<n;i++)
                {
                    if(isprime(a[i]))
                    {
                    if(a[i]>=k)
                    c++;
                    }
                }
                System.out.print(c);
                
            
        }
    }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Sample
+{
+    public static boolean isprime(int n)
+   { 
+      if(n==1)
+      return false;
+    for(int i=2;i<=Math.sqrt(n);i++)
+    {
+       if(n%i==0)
+       return false;
+    }
+     return true;
+  }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        
+            int n=s.nextInt();
+   
+            int a[]=new int[n];
+            for(int i=0;i<n;i++)
+            a[i]=s.nextInt();
+            int k=s.nextInt();
+            int c=0;
+            for(int i=0;i<n;i++)
+            {
+                if(isprime(a[i]))
+                {
+                if(a[i]>=k)
+                c++;
+                }
+            }
+            System.out.print(c);
+            
+        
+    }
+}
