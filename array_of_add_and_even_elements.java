e10e047a407dfb099a369a90ed5807f494abcaf2
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-10-24 22:15:07 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];
            for(int i=0;i<n;i++)
            a[i]=s.nextInt();
             int ar[]=new int[n];
             int j=0;
             for(int i=0;i<n;i++)
             {
                 if(a[i]%2!=0)
                 {
                     ar[j]=a[i];
                     j++;
                    
                 }
             }
                for(int i=0;i<n;i++)
             {
                 if(a[i]%2==0)
                 {
                     ar[j]=a[i];
                     j++;
                 }
                 
             }
             for(j=0;j<n;j++)
             System.out.print(ar[j]+" ");
        }
    }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+       
+        for(int i=0;i<n;i++)
+        a[i]=s.nextInt();
+         int ar[]=new int[n];
+         int j=0;
+         for(int i=0;i<n;i++)
+         {
+             if(a[i]%2!=0)
+             {
+                 ar[j]=a[i];
+                 j++;
+                
+             }
+         }
+            for(int i=0;i<n;i++)
+         {
+             if(a[i]%2==0)
+             {
+                 ar[j]=a[i];
+                 j++;
+             }
+             
+         }
+         for(j=0;j<n;j++)
+         System.out.print(ar[j]+" ");
+    }
+}
