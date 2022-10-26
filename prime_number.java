16befc1636e79df207050acf984cc98d6632c947
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-10-26 12:15:55 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int c=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             c++;
            }
            if(c==0)
            System.out.println("prime");
            else
             System.out.println("not a prime");
        }
    }

--- prime_number.java
+++ prime_number.java
@@ -1,30 +1,20 @@
-import java.util.Scanner;
-class PrimesorNot
+import java.util.*;
+class Sample
 {
-  public static boolean isPrime(int n)
-  {
-     int count=0;
-	 for(int i=2;i<=(int)Math.sqrt(n);i++)
-	 {
-	    if(n%i==0)
-		  count++;
-	 }
-	 if(count==0)
-	    return true;
-	 else
-	    return false;
-  }
-   public static void main(String args[])
-  {
-     Scanner sc=new Scanner(System.in);
-	 int n;
-	 n=sc.nextInt();
-	 if(isPrime(n))
-	   System.out.println("prime");
-	 else
-	   System.out.println("not a prime");
-	 
-	 sc.close();
-		    
-  }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int c=0;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            c++;
+            
+        }
+        if(c==0)
+        System.out.println("prime");
+        else
+         System.out.println("not a prime");
+    }
 }
