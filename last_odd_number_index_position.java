e84a22b1464ed62c4c4ba29f54556359c9d15728
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-11-01 22:24:24 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];
            for(int i=0;i<n;i++)
            a[i]=s.nextInt();
            int m=0;
             for(int i=n-1;i>=0;i--)
             {
                 if(a[i]!=0)
                 {
                 m=i;
                 break;
                 }
             }
             System.out.print(m);
            
        }
    }

--- last_odd_number_index_position.java
+++ last_odd_number_index_position.java
@@ -1,20 +1,24 @@
 import java.util.*;
-class sol
+class sample
 {
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int a[]=new int[n];
-        for(int  i=0;i<n;i++)
-            a[i]=s.nextInt();
-        int i;
-       
-           for(i=n-1;i<0;i--)
-           {
-           if(a[i]%2!=0)
-           break;
-       }
-        System.out.print(i);
+        
+        for(int i=0;i<n;i++)
+        a[i]=s.nextInt();
+        int m=0;
+         for(int i=n-1;i>=0;i--)
+         {
+             if(a[i]!=0)
+             {
+             m=i;
+             break;
+             }
+         }
+         System.out.print(m);
+        
     }
 }
