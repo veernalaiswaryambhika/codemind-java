e52739316464d359e4b12c638a154c6d178ba9b2
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-11-01 22:19:25 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];
            for(int i=0;i<n;i++)
            a[i]=s.nextInt();
            int max=a[0];
             for(int i=1;i<n;i++)
             {
                 if(a[i]>max)
                 max=a[i];
             }
             System.out.print(max);
            
        }
    }

--- maximum_of_an_array.java
+++ maximum_of_an_array.java
@@ -1,24 +1,21 @@
 import java.util.*;
-class Solution
+class sample
 {
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int a[]=new int[n];
+        
         for(int i=0;i<n;i++)
         a[i]=s.nextInt();
-       
-int temp=a[0];
-	 for(int i=0;i<n;i++)
-
-	{
-	 if(temp<a[i])
-	temp=a[i];
-				
-				
-}
-
-        System.out.println(temp);
+        int max=a[0];
+         for(int i=1;i<n;i++)
+         {
+             if(a[i]>max)
+             max=a[i];
+         }
+         System.out.print(max);
+        
     }
 }
