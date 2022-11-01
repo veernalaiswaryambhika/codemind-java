dc62553fc72e8e6a9a60bdc0b728404c88231be5
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-11-01 21:50:41 +0530

    import java.util.*; class Sammple {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];         int ss=0;         int s1=0;         for(int i=0;i<n;i++)         {         a[i]=s.nextInt();
                ss+=a[i];
               
            }
           
            System.out.print(ss);
        }
    }

---
+++ Sum_of_Array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Sammple
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        int ss=0;
+        int s1=0;
+        for(int i=0;i<n;i++)
+        {
+        a[i]=s.nextInt();
+       
+            ss+=a[i];
+           
+        }
+       
+        System.out.print(ss);
+    }
+}
