d4917b042be6f73301a8fea7f34ecb1b0dc230fd
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-11-01 22:17:14 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];
            for(int i=0;i<n;i++)
            a[i]=s.nextInt();
            int min=a[0];
             for(int i=1;i<n;i++)
             {
                 if(a[i]<a[0])
                 min=a[i];
             }
             System.out.print(min);
            
        }
    }

--- minimum_element_of_an_array.java
+++ minimum_element_of_an_array.java
@@ -1,24 +1,21 @@
 import java.util.*;
-class Solution
+class sample
 {
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int a[]=new int[n];
+        
         for(int i=0;i<n;i++)
         a[i]=s.nextInt();
-        int min=0;
-int temp=a[0];
-	 for(int i=0;i<n;i++)
-
-	{
-	 if(temp>a[i])
-	temp=a[i];
-				
-				
-}
-
-        System.out.println(temp);
+        int min=a[0];
+         for(int i=1;i<n;i++)
+         {
+             if(a[i]<a[0])
+             min=a[i];
+         }
+         System.out.print(min);
+        
     }
 }
