8c54faa32134eba80a42036cdb2c3f4be0934e83
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-11-01 22:35:20 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];
            for(int i=0;i<n;i++)
            a[i]=s.nextInt();
            int m=0;
             for(int i=0;i<n-2;i++)
             {
                 if(a[i]%2==0&&a[i+2]%2!=0||a[i]%2!=0&&a[i+2]%2==0)
                 {
                m++;
                 }
             }
             System.out.print(m);
            
        }
    }

--- count_numbers_between_even_and_odd_numbers_or_vice_versa.java
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.java
@@ -6,16 +6,18 @@ class sample
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int a[]=new int[n];
+        
         for(int i=0;i<n;i++)
         a[i]=s.nextInt();
-        int c=0;
-        for(int i=0;i<n-2;i++)
-        {
-            if((a[i]%2==0&&a[i+2]%2!=0)||(a[i]%2!=0&&a[i+2]%2==0))
-            {
-            c++;
-            }
-        }
-        System.out.print(c);
+        int m=0;
+         for(int i=0;i<n-2;i++)
+         {
+             if(a[i]%2==0&&a[i+2]%2!=0||a[i]%2!=0&&a[i+2]%2==0)
+             {
+            m++;
+             }
+         }
+         System.out.print(m);
+        
     }
 }
