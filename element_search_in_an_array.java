01e252588359080068132e92633648bbd01c7507
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-11-01 21:57:22 +0530

    import java.util.*; class Sammple {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];
            for(int i=0;i<n;i++)
            {
            a[i]=s.nextInt();
            
            }
            int k=s.nextInt();
            int f=0;
             for(int i=0;i<n;i++)
             {
                 if(a[i]==k)
                 {
                 f=1;
                 break;
                 }
             }
          if(f==1)
            System.out.print("True");
            
            else
             System.out.print("False");
            
        }
    }

--- element_search_in_an_array.java
+++ element_search_in_an_array.java
@@ -1,24 +1,33 @@
 import java.util.*;
-class sol
+class Sammple
 {
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int a[]=new int[n];
-        for(int  i=0;i<n;i++)
+        
+      
+        for(int i=0;i<n;i++)
+        {
         a[i]=s.nextInt();
-        int i,t=0;
-         int k=s.nextInt();
-       
-           for(i=0;i<n;i++)
-           {
-           if(a[i]==k)
-           t++;
-           
-       }if(t>0)
-        System.out.println("True");
+        
+        }
+        int k=s.nextInt();
+        int f=0;
+         for(int i=0;i<n;i++)
+         {
+             if(a[i]==k)
+             {
+             f=1;
+             break;
+             }
+         }
+      if(f==1)
+        System.out.print("True");
+        
         else
-           System.out.println("False");
+         System.out.print("False");
+        
     }
 }
