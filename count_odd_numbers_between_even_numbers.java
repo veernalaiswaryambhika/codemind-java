ae59c9edc43d8d02bed70ba5db5a49e9435175b2
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-11-01 22:40:15 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];
            for(int i=0;i<n;i++)
            a[i]=s.nextInt();
            int m=0;
             for(int i=0;i<n-2;i++)
             {
                 if(a[i]%2==0&&a[i+1]%2!=0&&a[i+2]%2==0)
                 {
                m++;
                 }
             }
             System.out.print(m);
            
        }
    }

--- count_odd_numbers_between_even_numbers.java
+++ count_odd_numbers_between_even_numbers.java
@@ -1,19 +1,23 @@
 import java.util.*;
-class sol
+class sample
 {
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int a[]=new int[n];
+        
         for(int i=0;i<n;i++)
-            a[i]=s.nextInt();
-        int c=0;
-        for(int i=0,j=1,k=2;k<n;k++,j++,i++)
-        {
-            if(a[i]%2==0 && a[j]%2!=0 && a[k]%2==0 )
-            c++;
-        }
-        System.out.print(c);
+        a[i]=s.nextInt();
+        int m=0;
+         for(int i=0;i<n-2;i++)
+         {
+             if(a[i]%2==0&&a[i+1]%2!=0&&a[i+2]%2==0)
+             {
+            m++;
+             }
+         }
+         System.out.print(m);
+        
     }
 }
