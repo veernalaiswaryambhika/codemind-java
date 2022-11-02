0be84106b208afa911572c6c10280c0542ec5ab1
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-11-02 21:42:56 +0530

    import java.util.*; class sol {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();             if(a[i]%2==0)                 System.out.print(a[i]+" ");         }         for(int i=0;i<n;i++)         {             if(a[i]%2!=0)                 System.out.print(a[i]+" ");         }
        }
    }

--- even_elements_followed_by_odd.java
+++ even_elements_followed_by_odd.java
@@ -1,35 +1,22 @@
 import java.util.*;
-class sample
+class sol
 {
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int a[]=new int[n];
-       
         for(int i=0;i<n;i++)
-        a[i]=s.nextInt();
-         int ar[]=new int[n];
-         int j=0;
-         for(int i=0;i<n;i++)
-         {
-             if(a[i]%2==0)
-             {
-                 ar[j]=a[i];
-                 j++;
-                
-             }
-         }
-            for(int i=0;i<n;i++)
-         {
-             if(a[i]%2!=0)
-             {
-                 ar[j]=a[i];
-                 j++;
-             }
-             
-         }
-         for(j=0;j<n;j++)
-         System.out.print(ar[j]+" ");
+        {
+            a[i]=s.nextInt();
+            if(a[i]%2==0)
+                System.out.print(a[i]+" ");
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2!=0)
+                System.out.print(a[i]+" ");
+        }
+        
     }
 }
