f9b78e7372f84ee75aea0a78046c9da3e85c2092
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-11-02 22:25:11 +0530

    import java.util.*; class sol {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];         int sum=0;         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();             sum+=a[i];         }         int avg=(int)Math.floor((float)sum/n);         int c=0;         for(int i=0;i<n;i++)         {             if(a[i]<=avg)                 c++;         }         System.out.print(c);
        }
    }

--- elements_smaller_than_average.java
+++ elements_smaller_than_average.java
@@ -1,25 +1,25 @@
 import java.util.*;
-class Solution
+class sol
 {
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int a[]=new int[n];
-        for(int i=0;i<n;i++)
-        a[i]=s.nextInt();
         int sum=0;
         for(int i=0;i<n;i++)
         {
-            sum=sum+a[i];
+            a[i]=s.nextInt();
+            sum+=a[i];
         }
+        int avg=(int)Math.floor((float)sum/n);
         int c=0;
-        int avg=sum/n;
-         for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         {
             if(a[i]<=avg)
-           c++;
+                c++;
         }
-         System.out.println(c);
+        System.out.print(c);
+        
     }
 }
